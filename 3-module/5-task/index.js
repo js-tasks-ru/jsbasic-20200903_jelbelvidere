794c4793569738537889c28515c444dcdba69259
Jeser-Belvidere <jelbelvidere@gmail.com>
2020-09-25 21:29:51 +0300

    Выполнена задача на поиск min/max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,19 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let fixedArrow = [];
+  let objArray = [];
+  let objectMaxMin = {};
+  let newArr = [];
+  let numericArray = [];
+  
+  newArr = str.split(/,| /);
+  newArr.forEach((item) => {if (item !== "") {fixedArrow.push(item)}})
+  numericArray = fixedArrow.map((item) => {return +item})
+  numericArray.forEach((item) => {if (!isNaN(item)) {objArray.push(item)}})
+
+  return objectMaxMin = {
+    min: Math.min(...objArray),
+    max: Math.max(...objArray),
+  };
 }
